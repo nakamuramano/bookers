00cf0db3ab94af38cf9df09d0d7fc6180d804636
nakamuramano <104703112+nakamuramano@users.noreply.github.com>
2022-05-07 13:35:19 +0000

    add3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,22 @@ class BooksController < ApplicationController
 
   def index
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    #redirect_to 'show_69728'
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-
   get '/books' => 'books#index'
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/books' => 'books#new'
+  post '/books' => 'books#create'
+  get 'show_69728' => 'books#show'
+
 end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
