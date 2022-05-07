72372070863643124c2d9fc3c822fdd9dab98a50
nakamuramano <104703112+nakamuramano@users.noreply.github.com>
2022-05-07 11:47:19 +0000

    add3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,5 @@
 class BooksController < ApplicationController
-  def new
-  end
-
-  def create
-  end
 
   def index
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
 
   get '/books' => 'books#index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
