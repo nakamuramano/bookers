36f74242400a1d42a2e1549febfdd9f5b83fd302
nakamuramano <104703112+nakamuramano@users.noreply.github.com>
2022-05-08 08:35:37 +0000

    add3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,34 @@
 class BooksController < ApplicationController
 
   def index
-  end
-
-  def new
+    @books = Book.all
     @book = Book.new
   end
 
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
   def create
-    book = Book.new(book_params)
-    book.save
-    #redirect_to 'show_69728'
+    @book = Book.new(book_params)
+    if @book.save
+       redirect_to book_path(@book)
+    else
+       render :new
+    end
+  end
+
+  def destroy
   end
 
 
   private
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title,:body)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get '/books' => 'books#index'
-  get '/books' => 'books#new'
-  post '/books' => 'books#create'
-  get 'show_69728' => 'books#show'
-
+  get 'books' => 'books#index'
+  get 'books ' => 'books#new'
+  get 'books/:id' => 'books#show', as: 'book'
+  post 'books' => 'books#create'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+    patch '/books/:id'  => 'books#update', as: 'update_book'
+    delete '/books/:id' => 'books#destroy', as: 'display_book'
 end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
