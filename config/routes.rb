0720ab669446787ba508f5f726debc16b0780097
nakamuramano <104703112+nakamuramano@users.noreply.github.com>
2022-05-08 09:35:08 +0000

    add3-12

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get 'books' => 'books#index'
-  get 'books ' => 'books#new'
-  get 'books/:id' => 'books#show', as: 'book'
-  post 'books' => 'books#create'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-    patch '/books/:id'  => 'books#update', as: 'update_book'
-    delete '/books/:id' => 'books#destroy', as: 'display_book'
+  get '/books' => 'books#index'
+  get '/books ' => 'books#new'
+  get '/books/:id' => 'books#show', as: 'book'
+  post '/books' => 'books#create'
+  get '/books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch '/books/:id'  => 'books#update', as: 'update_book'
+  delete '/books/:id' => 'books#destroy', as: 'destroy_book'
 end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
