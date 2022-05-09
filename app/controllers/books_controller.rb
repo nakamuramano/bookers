36a77cc2d473c7347d45c61d4f1c7c563bc574ab
nakamuramano <104703112+nakamuramano@users.noreply.github.com>
2022-05-09 08:26:50 +0000

    add3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,9 +11,9 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
-        @book.save
-       redirect_to book_path(@book)
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def destroy
@@ -31,6 +31,8 @@ class BooksController < ApplicationController
     book.update(book_params)
     redirect_to book_path(book.id)
   end
+
+
 
   private
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/books/:id' => 'books#show', as: 'book'
   post '/books' => 'books#create'
   get '/books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch '/books/:id'  => 'books#update', as: 'update_book'
+  patch '/books/:id' => 'books#update', as: 'update_book'
   delete '/books/:id' => 'books#destroy', as: 'destroy_book'
 end
 
