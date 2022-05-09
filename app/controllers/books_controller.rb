ad3fb0c3761596d563b57c26708825f2fa2bb4e3
nakamuramano <104703112+nakamuramano@users.noreply.github.com>
2022-05-09 12:33:25 +0000

    add3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,4 @@
 class BooksController < ApplicationController
-  protect_from_forgery
 
   def index
     @books = Book.all
@@ -11,9 +10,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), flash: 'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def destroy
@@ -27,9 +30,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), flash: 'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
 
